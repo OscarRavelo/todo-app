f56545e477982e7d14fb2f56b3147b34df002553
Oscar Ravelo <oscar91ravelo@gmail.com>
2024-11-11 23:01:45 +0100

    i break my app, fixing the drag and drop functionality

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,5 +1,5 @@
 import Dashboard from "@/components/dashboard";
-
+import { DragDropContext, Droppable } from "@hello-pangea/dnd";
 export default function Home() {
   return (
     <div className="bg-gray-100 p-6 border-red-500 border-t h-screen text-black overflow-hidden">
